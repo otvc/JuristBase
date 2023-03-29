36e6c875655b6b00deda46e6fccfbd30fc18bf53
otvc <olegerko048@gmail.com>
2023-03-29 19:18:34 +0300

    add metrics Precision, MP for ranking and change training with mlflow

--- notebooks/Experiments/ExperimentsBase.py
+++ notebooks/Experiments/ExperimentsBase.py
@@ -1,9 +1,19 @@
 import sys
+from typing import Union, Any
+
+import numpy as np
+import pandas as pd
 
 import torch
 from torch.utils.data import Dataset
+
 import matplotlib.pyplot as plt
+
 from sklearn.feature_extraction.text import TfidfVectorizer
+
+import nmslib
+
+from tqdm import tqdm
 
 from JuristEngine.Models import RetrieverDouble, TFIDFNet, LinearScore, CosineScore
 
@@ -36,10 +46,117 @@ def savefig_train_val(train_loss, val_loss, path, title_suffix=''):
     plt.savefig(path)
 
 def parse_experiments_args():
-    lr = float(sys.argv[1]) if len(sys.argv) > 1 else 1e-3
-    epochs = int(sys.argv[2]) if len(sys.argv) > 2 else 10
+    lr = float(sys.argv[1]) if len(sys.argv) > 1 else 3e-4
+    epochs = int(sys.argv[2]) if len(sys.argv) > 2 else 20
     batch_size = int(sys.argv[3]) if len(sys.argv) > 3 else 32
-    max_quest_features = int(sys.argv[4]) if len(sys.argv) > 4 else 1000
-    max_doc_features = int(sys.argv[5]) if len(sys.argv) > 5 else 1000
+    max_quest_features = int(sys.argv[4]) if len(sys.argv) > 4 else 10000
+    max_doc_features = int(sys.argv[5]) if len(sys.argv) > 5 else 10000
     return lr, epochs, batch_size, max_quest_features, max_doc_features
+
+'''
+
+`params`:
+    `doc_vectors`:Union[torch.tensor, np.ndarray]: data with shapes (n, features);
+    `method`:str: see in `nmslib.init` documentation;
+    `space`:str: see in `nmslib.init` documentation.
+
+return:
+    created nmslib index.
+'''
+def create_nms_index(doc_vectors:Union[torch.tensor, np.ndarray], method:str = 'hnsw', space:str = 'cosinesimil'):
+    index = nmslib.init(method = method, space = space)
+    index.addDataPointBatch(doc_vectors)
+    index.createIndex()
+    return index
+
+'''
+`params`:
+    `data`:pd.DataFrame: dataframe, which contain text of question;
+    `model`:torch.nn.Module: model for extraction question features;
+    `index`:: index from nmslib for searching;
+    `data_doc:pd.DataFrame: dataframe, which contain text of documents;
+    `target`:Any: name of column from data;
+    `K`:int: count of searching documents.
+return
+    1d tensor with shapes which equal data.shape[0]
+'''
+def find_docs_for_queries(data:pd.DataFrame, model:torch.nn.Module, index, data_doc, target:Any = 'question', target_doc = 'article_id', K = 3):
+    indeces = []
+    with torch.no_grad():
+        for i in tqdm(data.index):
+            part_ids, _ = index.knnQuery(model(data.loc[i:i+1][target]), k = K)
+            indeces.append(data_doc.iloc[part_ids].index)
+    return torch.tensor(indeces)
+
+'''
+`params`:
+    `data`:pd.DataFrame: dataframe which contain needed feature with text;
+    `model`:torch.nn.Module: model which obtain batch with `STR` and output torch features;
+    `target`:Any: value of column with text information from `data` argument;
+    `batch_size`:int: size of batch for feature extraction.
+
+return:
+    torch.Tensor with shape (data.shape[0], N1, N2, ..., Nn), where other demension dependend 
+    from size outputed model features.
+'''
+def create_doc_features(data:pd.DataFrame, model:torch.nn.Module, target:Any = 'paragraph', batch_size:int = 256):
+    were_training = model.training
+    if model.training:
+        model.eval()
+
+    total_batches = data.shape[0]//batch_size
+    embedded_vecs = []
+    with torch.no_grad():
+        for i in tqdm(range(total_batches)):
+            batched_vecs = model(data.iloc[i*batch_size:(i+1)*batch_size][target])
+            embedded_vecs.extend(batched_vecs)
+
+    if were_training:
+        model.train()
+
+    return embedded_vecs
+
+'''
+Calculating Precision@K
+
+`params`:
+    `predictions`:Union[torch.tensor, np.ndarray]: values, which predicted your algs;
+    `ground_truth`:Union[torch.tensor, np.ndarray]: ground truth values from your dataset;
+    `K`:int: hyperparameter of Precision@K metrics, which define how much of first K data 
+             should be get for evaluating (K value should be less then size of `predictions`);
+    `check_limit`:bool: using for limiting K value if `ground_truth` or `predictions` less then
+                        `K` will equal min size.
+
+return: 
+    metric value
+'''
+def PrecisionK(predictions:Union[torch.tensor, np.ndarray], ground_truth:Union[torch.tensor, np.ndarray], K:int = 5, check_limit:bool = True):
+    ground_truth = torch.tensor(ground_truth)
+    if check_limit:
+        K = min(min(ground_truth.shape[0], predictions.shape[0]), K)
+    ground_truth = pd.DataFrame(ground_truth[:K])
+    predictions = pd.DataFrame(predictions[:K])
+    predictions.index = predictions[0].values
+    ground_truth.index = ground_truth[0].values
+    gp_join = predictions.join(ground_truth, how = 'inner', lsuffix='-')
+    return gp_join.shape[0]/ground_truth.shape[0]
+
+
+'''
+Calculating Mean Precision@K
+Because for my model ranking is not important and important that values in topk, 
+that MAP it's not valueble, but Mean Precision is corresponding for this task
+
+`params`:
+    `queries_predictions`:Union[torch.tensor, np.ndarray]: predictions for all queries;
+    `queries_ground_truth`:Union[torch.tensor, np.ndarray]: ground_truth predictions for all queries
+    `MAX_K`:int: maximum k for precision
+return:
+    float mean value
+'''
+def MP(queries_predictions:Union[torch.tensor, np.ndarray], queries_ground_truth:Union[torch.tensor, np.ndarray], MAX_K:int = 5):
+    return torch.tensor([PrecisionK(predictions, ground_truth, K = cur_k)
+                         for cur_k in range(1, MAX_K + 1)
+                         for predictions, ground_truth
+                         in zip(queries_predictions, queries_ground_truth)]).mean(-1).mean(-1)
 
--- notebooks/Experiments/train_cosine_emb_base_line.py
+++ notebooks/Experiments/train_cosine_emb_base_line.py
@@ -13,6 +13,8 @@ import torch
 from torch import nn 
 from torch.utils.data import Dataset, DataLoader
 
+import numpy as np
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import roc_auc_score, roc_curve, accuracy_score
 
@@ -20,7 +22,7 @@ from sklearn.metrics import roc_auc_score, roc_curve, accuracy_score
 from JuristEngine import Models 
 from JuristEngine.Models import RetrieverDouble, TFIDFNetEmb, CosineScore, Twins
 from JuristEngine import Instruments
-from ExperimentsBase import load_ttv_datasets, savefig_train_val, parse_experiments_args
+from ExperimentsBase import load_ttv_datasets, savefig_train_val, parse_experiments_args, create_doc_features, create_nms_index, find_docs_for_queries, MP
 
 import mlflow
 import mlflow.pytorch
@@ -46,7 +48,7 @@ def create_model(train_dataset, df_art_to_par, max_quest_features, max_doc_featu
     doc_retriever = TFIDFNetEmb(vectorizer_doc)
     base_retriever = Twins(tfidf_retriever, doc_retriever)
 
-    return base_retriever
+    return base_retriever, tfidf_retriever, doc_retriever
 
 if __name__ == '__main__':
     warnings.filterwarnings('ignore')
@@ -58,9 +60,10 @@ if __name__ == '__main__':
     path_train_val_plot = graphics_dir + 'losses_' + start_time + '.png'
     
     base_path, train_jurist, test_jurist, val_jurist = './data/datasets/', 'train_article_match.pt', 'test_article_match.pt', 'val_article_match.pt'
-    path_df_art_to_par = base_path+'df_art_to_par.csv'
+    path_df_art_to_par = base_path + 'df_art_to_par.csv'
 
     lr, epochs, batch_size, max_quest_features, max_doc_features =  parse_experiments_args()
+    MAX_K = 5
     
     train_dataset, test_dataset, val_dataset = load_ttv_datasets(base_path + train_jurist, base_path + test_jurist, base_path + val_jurist)
     train_dataloader = DataLoader(train_dataset, batch_size = batch_size, shuffle = True)  
@@ -69,15 +72,28 @@ if __name__ == '__main__':
     
     df_art_to_par = pd.read_csv(path_df_art_to_par, index_col = 'article_id')
 
-    model = create_model(train_dataset, df_art_to_par, max_quest_features, max_doc_features)
+    model, qest_retriever, doc_retriever = create_model(train_dataset, df_art_to_par, max_quest_features, max_doc_features)
 
     with mlflow.start_run():
         criterion = nn.CosineEmbeddingLoss()
         
         optimizer = torch.optim.Adam(model.parameters(), lr = lr)
-
-        output = Instruments.train_tfidf_cosine_score(model, train_dataloader, val_dataloader,optimizer, criterion, 
+        output = Instruments.train_tfidf_cosine_score(model, train_dataloader, val_dataloader, optimizer, criterion, 
                                                       epochs = epochs, plot_loss = False)
+
+        features = create_doc_features(df_art_to_par, doc_retriever)
+        index = create_nms_index(features)
+
+        df_ground_truth = test_dataset.dataset[test_dataset.dataset.score > 0]
+        df_ground_truth_articles = df_ground_truth[['question_id', 'article_id']].groupby('question_id').agg(list)
+
+        duplicated = df_ground_truth[['question', 'question_id']].duplicated(keep='last')
+        duplicated_indeces = duplicated.index[duplicated]
+        test_questions = df_ground_truth.loc[duplicated_indeces]
+        predictions = find_docs_for_queries(test_questions, qest_retriever, index, df_art_to_par)
+        print(df_ground_truth_articles['article_id'].values[:3])
+        MP_metric = MP(predictions, df_ground_truth_articles['article_id'].values, MAX_K=MAX_K)
+        print(MP_metric)
 
         train_loss = output['train_loss']
         val_loss = output['val_loss']
@@ -85,4 +101,5 @@ if __name__ == '__main__':
         savefig_train_val(train_loss, val_loss, path_train_val_plot)
         mlflow.log_artifact(path_train_val_plot)
 
+        mlflow.log_metric(f'MP{MAX_K}', MP_metric)
         mlflow.pytorch.log_model(model, 'tf_idf_cosine_score.pt', registered_model_name = 'tf_idf_cosine_score.pt')
